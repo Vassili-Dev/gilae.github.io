a07263a2c9923516d8353a6e3f91893de0ea21eb
NickolaiVassiliev <Nickolai.Vassiliev@interadcorp.com>
2018-10-18 11:48:51 -0400

    Made accessible

--- assets/ik_sortable.js
+++ assets/ik_sortable.js
@@ -1,8 +1,11 @@
 ;(function ( $, window, document, undefined ) {
- 
-var pluginName = "ik_sortable",
-	defaults = {};
-	 
+	
+	var pluginName = "ik_sortable",
+	modifier = navigator.platform.indexOf('Mac') > -1 ? 'Command' : 'Control',
+	defaults = {
+		'instructions': 'Use arrow keys to select a list item,  ' + modifier + ' + arrow keys to move it to a new position.'
+	};
+	
 	function Plugin( element, options ) {
 		
 		this.element = $(element);
@@ -12,7 +15,7 @@ var pluginName = "ik_sortable",
 		
 		this.init();
 	}
-	 
+	
 	Plugin.prototype.init = function () {
 		
 		var $elem, plugin, id, total;
@@ -20,35 +23,51 @@ var pluginName = "ik_sortable",
 		plugin = this;
 		id = 'sortable_' + $('.ik_sortable').length;
 		$elem = this.element.attr({
-			'id': id
+			'id': id,
+			'role': 'list',
+			'tabindex': 0,
+			'aria-labelledby': id + '_instructions'
 		})
 		.wrap('<div class="ik_sortable"></div>').before(plugin.temp);
-			
-		total = $elem.children('li').length;
-			
-		plugin.items = $elem.children('li').each( function(i, el) {
-			
-			$(el).attr({
-				'draggable': true,
-				'id': id + '_' + i
-			});
+		
+		$('<div/>') // add div element to be used with aria-describedby attribute of the menu
+		    .text(plugin.options.instructions) // get instruction text from plugin options
+		    .addClass('ik_readersonly') // hide element from visual display
+		    .attr({
+		    	'id': id + '_instructions',
+		    'aria-hidden': 'true'  // hide element from screen readers to prevent it from being read twice
 		})
-		.on('dragstart', {'plugin': plugin}, plugin.onDragStart)
-		.on('drop', {'plugin': plugin}, plugin.onDrop)
-		.on('dragend', {'plugin': plugin}, plugin.onDragEnd)
-		.on('dragenter', {'plugin': plugin}, plugin.onDragEnter)
-		.on('dragover', {'plugin': plugin}, plugin.onDragOver)
-		.on('dragleave', {'plugin': plugin}, plugin.onDragLeave);
+		    .appendTo($elem);
+
+		    total = $elem.children('li').length;
+		    
+		    plugin.items = $elem.children('li').each( function(i, el) {
+		    	
+		    	$(el).attr({
+		    		'draggable': true,
+		    		'id': id + '_' + i,
+		    		'role': 'listitem',
+		    		'aria-label': $(el).text() + ' ' + (i + 1) + ' of ' + total + ' movable',
+		    		'tabindex': i > 0 ? -1 : 0
+		    	});
+		    })
+		    .on('dragstart', {'plugin': plugin}, plugin.onDragStart)
+		    .on('drop', {'plugin': plugin}, plugin.onDrop)
+		    .on('dragend', {'plugin': plugin}, plugin.onDragEnd)
+		    .on('dragenter', {'plugin': plugin}, plugin.onDragEnter)
+		    .on('dragover', {'plugin': plugin}, plugin.onDragOver)
+		    .on('dragleave', {'plugin': plugin}, plugin.onDragLeave)
+		    .on('keydown', {'plugin': plugin}, plugin.onKeyDown);
+		    
+		    
+		};
 		
-		
-	};
-	
 	// dragged item
 	
 	Plugin.prototype.onDragStart = function (event) {
 		
 		var plugin, $me;
-				
+		
 		plugin = event.data.plugin;
 		event = event.originalEvent || event;
 		$me = $(event.currentTarget);
@@ -84,7 +103,7 @@ var pluginName = "ik_sortable",
 	Plugin.prototype.onDragEnd = function (event) {
 		
 		var plugin;
-				
+		
 		plugin = event.data.plugin;
 		plugin.element.find('.dragover').removeClass('dragover');
 		
@@ -121,27 +140,76 @@ var pluginName = "ik_sortable",
 		
 	};
 	
-	Plugin.prototype.resetNumbering = function (plugin) {
+	Plugin.prototype.onKeyDown = function (event) {
 		
-		plugin.items = plugin.element.children('li');
+		var plugin, $me, currIndex, nextIndex;
 		
-		plugin.items.each( function(i, el) {
-			var $me = $(el);
-		});
+		plugin = event.data.plugin;
+		$me = $(event.currentTarget);
+		currentIndex = plugin.items.index(event.currentTarget);
 		
-	}
+		switch (event.keyCode) {
+			
+			case ik_utils.keys.down:
+			
+			plugin.items.attr({'tabindex': -1});
+			
+			if(currentIndex < plugin.items.length - 1) {
+                    if(event.ctrlKey || event.metaKey) { // move item down
+                    	$me.insertAfter( $me.next() );
+                    	$me.attr({'tabindex': 0}).focus();
+                    	plugin.resetNumbering(plugin);
+                    } else { // move focus to the next item
+                    	$me.next().attr({'tabindex': 0}).focus();
+                    }
+                }
+                
+                break;
+                
+                case ik_utils.keys.up:
+                
+                plugin.items.attr({'tabindex': -1});
+                
+                if(currentIndex > 0) {
+                    if(event.ctrlKey || event.metaKey) { // move item up
+                    	$me.insertBefore( $me.prev() );
+                    	$me.attr({'tabindex': 0}).focus();
+                    	plugin.resetNumbering(plugin);
+                    } else { // move focus to the previous item
+                    	$me.prev().attr({'tabindex': 0}).focus();
+                    }
+                }
+                
+                break;
+                
+            }
+            
+        };
 
-	$.fn[pluginName] = function ( options ) {
-		
-		return this.each(function () {
-			
-			if ( !$.data(this, pluginName )) {
-				$.data( this, pluginName,
-				new Plugin( this, options ));
-			}
-			
-		});
-		
-	}
- 
-})( jQuery, window, document );
+        Plugin.prototype.resetNumbering = function (plugin) {
+        	
+        	plugin.items = plugin.element.children('li');
+        	
+        	plugin.items.each( function(i, el) {
+        		var $me = $(el);
+        		$me.attr({
+        			'aria-label': $me.text() + ' ' + (i + 1) + ' of ' + plugin.items.length + ' movable'
+        		});
+        	});
+        	
+        }
+
+        $.fn[pluginName] = function ( options ) {
+        	
+        	return this.each(function () {
+        		
+        		if ( !$.data(this, pluginName )) {
+        			$.data( this, pluginName,
+        				new Plugin( this, options ));
+        		}
+        		
+        	});
+        	
+        }
+        
+    })( jQuery, window, document );
